5d4249dd561d7563f5f34c0c65e274fb57397509
awesome94 <david.kironde@andela.com>
2020-01-09 13:53:01 +0300

    add some documentation to the binary search algorithm

--- binarysearch.py
+++ binarysearch.py
@@ -1,8 +1,14 @@
 def BinarySearch(arr, tar):
+    """
+    The binary search algorithm runs in o(logn) time
+    it is used to find a specific element in a sorted array.
+    Gets the midpoint of a given array and hence compare the target item with the item at mid index
+    if the item is < than the mid. We then find the next mid from the current positon  and vice versa
+    """
     L = len(arr)-1
     i = 0
     while i <= L:
-        mid = i + (L - i)//2
+        mid = i + (L - i)//2 # better way to calculate mid point to avoid overflow
         if arr[mid] == tar:
             return mid
         elif arr[mid] < tar:
