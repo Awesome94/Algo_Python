e96675367365a0dd08eb0c68cec9e35737fe2e87
awesome94 <david.kironde@andela.com>
2020-01-13 18:59:19 +0300

    refactor find duplicates code

--- findDuplicates.py
+++ findDuplicates.py
@@ -1,7 +1,13 @@
 def findDuplicates(arr):
+    mid  = len(arr)//2
+    return[x for x in arr[:mid] if x in arr[mid:]]
+
+def findDuplicates2(arr):
     for x in set(arr):
         if arr.count(x) <= 1:
             arr.pop(arr.index(x))
     return list(set(arr))
+    
 arr = [4,3,2,7,8,9,8,3,2,1]
 print(findDuplicates(arr))
+print(findDuplicates2(arr))
