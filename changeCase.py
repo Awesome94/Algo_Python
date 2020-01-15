2230fa61f9369f9cefc9283db2edcb19e4edac5c
awesome94 <david.kironde@andela.com>
2020-01-16 00:22:49 +0300

    add function to convert uppercase to lower case

--- changeCase.py
+++ changeCase.py
@@ -1,4 +1,4 @@
-def toUpper(string):
+def toUpperCase(string):
     upper = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     lower = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     str_lst = [s for s in string]
@@ -7,4 +7,14 @@ def toUpper(string):
             str_lst[str_lst.index(x)] = upper[lower.index(x)]
     return "".join(str_lst)
 
-print(toUpper("This is it"))
+def toLowerCase(string):
+    upper = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    lower = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    str_lst = [ x for x in string]
+    for s in string:
+        if s in upper:
+            str_lst[str_lst.index(s)] = lower[upper.index(s)]
+    return "".join(str_lst)
+
+print(toUpperCase("This is it"))
+print(toLowerCase("THIS IS IT"))
