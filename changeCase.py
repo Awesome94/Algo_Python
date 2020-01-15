d1f019b6ac1b26387f07b0b02f4812da5c9b4e28
awesome94 <david.kironde@andela.com>
2020-01-16 00:18:16 +0300

    add a function that changes letters from lower case to upper case

---
+++ changeCase.py
@@ -0,0 +1,10 @@
+def toUpper(string):
+    upper = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    lower = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    str_lst = [s for s in string]
+    for x in string:
+        if x in lower:
+            str_lst[str_lst.index(x)] = upper[lower.index(x)]
+    return "".join(str_lst)
+
+print(toUpper("This is it"))
