601e6a7ac44e2c6ee24bc3a2b5f7348e769dea26
awesome94 <david.kironde@andela.com>
2020-01-16 12:35:33 +0300

    replace long alphabet lists with strings

--- changeCase.py
+++ changeCase.py
@@ -1,20 +1,22 @@
 def toUpperCase(string):
-    upper = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    lower = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     str_lst = [s for s in string]
     for x in string:
         if x in lower:
             str_lst[str_lst.index(x)] = upper[lower.index(x)]
     return "".join(str_lst)
+# o(n) runtime and o(n) space
 
 def toLowerCase(string):
-    upper = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    lower = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     str_lst = [ x for x in string]
     for s in string:
         if s in upper:
             str_lst[str_lst.index(s)] = lower[upper.index(s)]
     return "".join(str_lst)
+# o(n) runtime and o(n) space   
 
 print(toUpperCase("This is it"))
 print(toLowerCase("THIS IS IT"))
