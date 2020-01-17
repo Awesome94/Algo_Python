3e29e0133d61c7bef6da68140e4bd6271af684d3
awesome94 <david.kironde@andela.com>
2020-01-17 09:44:19 +0300

    modify logic

--- commonChars.py
+++ commonChars.py
@@ -4,7 +4,6 @@ def common_characters(lst):
     lst1 = lst[0]
     n = len(lst1)
     final = {}
-    flist = []
     while i < n:
         for x in range(1, len(lst)):
             if lst1[i] in lst[x]:
@@ -13,13 +12,10 @@ def common_characters(lst):
                 lst[x] = lst[x][:idx] + lst[x][(idx+1):]
             else:
                 commonChar = ""
+      
             final[i] = commonChar
         i+=1
-    for _, values in final.items():
-        if values:
-            flist.append(values)
-    return flist
-
+    return[x for x in final.values() if x]
 print(common_characters(['google', 'facebook', 'youtube']))
 
 # o(n)space o(n)time complexity
