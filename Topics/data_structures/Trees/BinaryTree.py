00715807996307b4105712698826981b2112c1b5
awesome94 <david.kironde@andela.com>
2020-02-10 17:31:09 +0300

    add find value logic

--- Topics/data_structures/Trees/BinaryTree.py
+++ Topics/data_structures/Trees/BinaryTree.py
@@ -5,15 +5,44 @@ class Node:
         self.left = None
     
     def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else: 
+                    self.left.insert(data)
+            elif data > self.data:
+                if not self.right:
+                    self.right = Node(data)
+                else: 
+                    self.right.insert(data)
+    
+    def findval(self, data):
+        if not self.data:
+            return " no data to search"
         if data < self.data:
-            if not self.data.left:
-                self.data.left = Node(data)
-            else: 
-                self.insert(data)
+            if not self.left:
+                return "not found at all"
+            return self.left.findval(data)
         elif data > self.data:
-            if not self.data.right:
-                self.data.right = Node(data)
-            else: 
-                self.insert(data)
+            if not self.right:
+                return "not found on the right"
+            return self.right.findval(data)
+        else:
+            print(self.data, "is found")
         
+
+    def print_tree(self, data):
+        if self.left:
+            self.left.print_tree(data)
+        print(self.data)
+        if self.right:
+            self.right.print_tree(data)
             
+root = Node(12)
+root.insert(6)
+root.insert(4)
+root.insert(3)
+
+print(root.findval(7))
+print(root.findval(12))
