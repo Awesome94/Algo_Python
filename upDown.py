ef99b8f1914c54b9a5e88f0e31b9164e5c3d675d
awesome94 <david.kironde@andela.com>
2020-02-25 10:25:16 +0300

    up down arr of numbers solution in python 3

---
+++ upDown.py
@@ -0,0 +1,33 @@
+def Oneup_Onedown(arr):
+    arr.sort()
+    x = 0
+    direction = True
+    n = len(arr)-1
+    while x < n:
+        j = x+1
+        if direction and arr[x] < arr[j]:
+            direction = False
+            x += 1
+            continue
+        elif direction and arr[x] >= arr[j]:
+            arr[x] -= 1
+            direction = True
+
+        if not direction and arr[x] > arr[j]:
+            x += 1
+            direction = True
+            continue
+        elif not direction and arr[x] <= arr[j]:
+            arr[j] -= 1
+            direction = False
+    return arr
+
+
+arr = [5, 4, 5, 6, 7]
+arr = [5, 4, 4, 4, 3, 6]
+c = [5, 6, 4, 4, 3, 4]
+arr2 = [3, 3, 3, 3, 3]
+# arr = [2, 4, 4, 4, 3, 2]
+[2, 2, 3, 4, 4, 4, 4]
+2, 4, 2, 4, 3, 4, 3, 4
+print(adNums(arr))
